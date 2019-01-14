08da5acf60fad6f57040e1a3e6617b8955ddb1d4
Reka <rekapap28@gmail.com>
2019-01-14 19:21:42 +0000

    Initial Commit

---
+++ IAmRich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by Réka Pap on 14/01/2019.
+//  Copyright © 2019 rekapap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
